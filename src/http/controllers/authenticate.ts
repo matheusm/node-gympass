085b2abee7eb69ccee0c1c46a9e12568d60b46ef
Matheus Martins <matheusms280100@gmail.com>
2023-08-08 19:05:27 -0300

    feat: add base of jwt auth

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -14,7 +14,20 @@ export async function authenticate(req: FastifyRequest, res: FastifyReply) {
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await res.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return res.status(200).send({
+      token,
+    })
   } catch (error) {
     if (error instanceof InvalidCredencialsError) {
       return res.status(400).send({ message: error.message })
@@ -22,6 +35,4 @@ export async function authenticate(req: FastifyRequest, res: FastifyReply) {
 
     throw error
   }
-
-  return res.status(200).send()
 }
