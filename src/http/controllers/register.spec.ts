ff443a36814741428a5bfc64de084955d9ec793b
Matheus Martins <matheusms280100@gmail.com>
2023-09-19 23:04:04 -0300

    adjust vitest config and add a register spec e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,27 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { FastifyInstance } from 'fastify'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  let app: FastifyInstance
+
+  beforeAll(async () => {
+    app = (await import('@/app')).app
+
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Johm Doe',
+      email: 'johndoe@ex.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
